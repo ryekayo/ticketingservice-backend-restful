e31d527d35cd6263ad8959a621bf946980afc912
Tom Holmes Jr. <tom@tomholmes.net>
2016-05-19 09:40:44 -0400

    fixed CustomerInformationController

--- ticketingsystem-backend-ws/src/main/java/org/ticketingsystem/backend/ws/CustomerInformationController.java
+++ ticketingsystem-backend-ws/src/main/java/org/ticketingsystem/backend/ws/CustomerInformationController.java
@@ -10,38 +10,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ticketingsystem.entity.CustomerInformationEntity;
 import com.ticketingsystem.backend.service.CustomerInformationService;
+import com.ticketingsystem.entity.CustomerInformationEntity;
 
 @Controller
 @RequestMapping("/customer")
-public class CustomerInformationController {
+public class CustomerInformationController
+{
 
-	private final static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-	
-	@Autowired
-	private CustomerInformationService service;
-	
-	@RequestMapping(value="", method = RequestMethod.GET, headers = "Accept=application/json")
-	public @ResponseBody ArrayList<CustomerInformationEntity> getCustomerIdList()
-	{
-		System.out.println("CustomerInformationController : getCustomerIdList : START");
-		ArrayList<CustomerInformationEntity> customer = (ArrayList) service.getAllCustomerInformation();
-		return customer;
-	}
-	@RequestMapping(value="/", method = RequestMethod.GET, headers = "Accept=application/json")
-	public @ResponseBody ArrayList<CustomerInformationEntity> getAllCustomers()
-	{
-		System.out.println("CustomerInformationController : getCustomerIdList : START");
-		ArrayList<CustomerInformationEntity> customer = (ArrayList) service.getAllCustomerInformation();
-		return customer;
-	}
-	@RequestMapping(value="/customerId/{customerId}", method = RequestMethod.GET, headers = "Accept=applicaton/json")
-	public @ResponseBody CustomerInformationEntity getCustomerByCustomerId(@PathVariable("customerId") long customerId)
-	{
-		System.out.println("CustomerInformationController : getCustomerByCustomerId : customerId = " + customerId);
-		CustomerInformationEntity customer = service.getEmailHistoryById(customerId);
-		System.out.println("CustomerInformationController : getCustomerByCustomerId : CustomerInformationEntity = " + customer);
-		return customer;
-	}
+    private final static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    private CustomerInformationService service;
+
+    @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
+    public @ResponseBody ArrayList<CustomerInformationEntity> getCustomerIdList()
+    {
+        System.out.println("CustomerInformationController : getCustomerIdList : START");
+        ArrayList<CustomerInformationEntity> customer = (ArrayList) service.getAllCustomerInformation();
+        return customer;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
+    public @ResponseBody ArrayList<CustomerInformationEntity> getAllCustomers()
+    {
+        System.out.println("CustomerInformationController : getCustomerIdList : START");
+        ArrayList<CustomerInformationEntity> customer = (ArrayList) service.getAllCustomerInformation();
+        return customer;
+    }
+
+    @RequestMapping(value = "/customerId/{customerId}", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+    public @ResponseBody CustomerInformationEntity getCustomerByCustomerId(@PathVariable("customerId") long customerId)
+    {
+        System.out.println("CustomerInformationController : getCustomerByCustomerId : customerId = " + customerId);
+        CustomerInformationEntity customer = service.getEmailHistoryById(customerId);
+        System.out.println("CustomerInformationController : getCustomerByCustomerId : CustomerInformationEntity = " + customer);
+        return customer;
+    }
 }
